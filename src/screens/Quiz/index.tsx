ad5a93cb53c36b544774b4cc28efeaf6bfd0e67c
Marcelogreick <mgreick25@gmail.com>
2023-10-24 21:07:25 -0300

    criando overlay com skia

--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -26,6 +26,7 @@ import Animated, {
 import { GestureDetector, Gesture } from "react-native-gesture-handler";
 import { ProgressBar } from "../../components/ProgressBar";
 import { THEME } from "../../styles/theme";
+import { OverlayFeedback } from "../../components/OverlayFeedback";
 
 interface Params {
   id: string;
@@ -44,6 +45,8 @@ export function Quiz() {
   const [alternativeSelected, setAlternativeSelected] = useState<null | number>(
     null
   );
+
+  const [statusReply, setStatusReply] = useState(0);
 
   const shake = useSharedValue(0);
   const scrollY = useSharedValue(0);
@@ -90,8 +93,10 @@ export function Quiz() {
     }
 
     if (quiz.questions[currentQuestion].correct === alternativeSelected) {
+      setStatusReply(1);
       setPoints((prevState) => prevState + 1);
     } else {
+      setStatusReply(2);
       shakeAnimation();
     }
 
@@ -213,6 +218,7 @@ export function Quiz() {
 
   return (
     <View style={styles.container}>
+      <OverlayFeedback status={statusReply} />
       <Animated.View style={fixedProgressBarStyles}>
         <Text style={styles.title}>{quiz.title}</Text>
 
